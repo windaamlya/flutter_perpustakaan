1ec3c9130b9ebb63ba04eb140edfe40ab7a3ec8f
windaamalya <147890995+windaamalya@users.noreply.github.com>
2024-11-21 14:04:17 +0700

    commit kedua

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,20 +1,16 @@
 import 'package:flutter/material.dart';
-// import 'package:supabase/supabase.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-
-
-
 
 class BookListPage extends StatefulWidget {
   const BookListPage({super.key});
 
   @override
-  _BookListPageState createState() =>_BookListPageState();
-
+  _BookListPageState createState() => _BookListPageState();
 }
 
-class  _BookListPageState extends State<BookListPage> {
+class _BookListPageState extends State<BookListPage> {
   List<Map<String, dynamic>> books = [];
+  bool isLoading = true;
 
   @override
   void initState() {
@@ -23,59 +19,275 @@ class  _BookListPageState extends State<BookListPage> {
   }
 
   Future<void> fetchBooks() async {
-    final response = await Supabase.instance.client
-        .from('books')
-        .select();
-
-        setState(() {
-          books = List<Map<String, dynamic>>.from(response);
-        });
+    setState(() {
+      isLoading = true;
+    });
+    try {
+      final response = await Supabase.instance.client.from('books').select();
+      setState(() {
+        books = List<Map<String, dynamic>>.from(response);
+        isLoading = false;
+      });
+    } catch (e) {
+      print('Error fetching books: $e');
+      setState(() {
+        isLoading = false;
+      });
+    }
   }
+
+  Future<void> deleteBook(int id) async {
+    await Supabase.instance.client.from('books').delete().eq('id', id);
+    fetchBooks();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Daftar Buku'),
+        title: const Text(
+          'Daftar Buku',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
         actions: [
           IconButton(
-          icon: Icon(Icons.refresh),
-          onPressed: fetchBooks,
+            icon: const Icon(Icons.refresh),
+            onPressed: fetchBooks,
           )
         ],
+        backgroundColor: Colors.brown[200],
       ),
-      body: books.isEmpty
-          ? Center(child: CircularProgressIndicator()) 
-          : ListView.builder(
-            itemCount: books.length,
-            itemBuilder: (context, index) {
-              final book = books[index];
-              return ListTile(
-              title: Text(book['title']?? 'No Title', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                Text(book['author'] ?? 'No Author', style: TextStyle(fontSize: 14, fontStyle: FontStyle.italic)),
-                Text(book['description'] ?? 'No Description', style: TextStyle(fontSize: 13, fontStyle: FontStyle.italic)),
-              ]
-              ),
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                   IconButton(icon: Icon(Icons.edit, color: Colors.blue),
-                   onPressed: (){
-                   Navigator.pop(context);
-                },
-                ),
-                IconButton(
-                  icon: Icon(Icons.delete, color: Colors.red,
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : books.isEmpty
+              ? const Center(
+                  child: Text(
+                    'Tidak ada buku tersedia',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
                 )
-                ]
+              : ListView.builder(
+                  padding: const EdgeInsets.all(8),
+                  itemCount: books.length,
+                  itemBuilder: (context, index) {
+                    final book = books[index];
+                    return Card(
+                      elevation: 4,
+                      margin: const EdgeInsets.symmetric(vertical: 8),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(12),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              book['title'] ?? 'No Title',
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20),
+                            ),
+                            const SizedBox(height: 4),
+                            Text(
+                              book['author'] ?? 'No Author',
+                              style: const TextStyle(
+                                  fontSize: 16,
+                                  fontStyle: FontStyle.italic,
+                                  color: Colors.grey),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              book['description'] ?? 'No Description',
+                              style: const TextStyle(
+                                fontSize: 14,
+                                height: 1.4,
+                              ),
+                              textAlign: TextAlign.justify,
+                            ),
+                            const SizedBox(height: 12),
+                            const Divider(),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                IconButton(
+                                  icon: const Icon(Icons.edit,
+                                      color: Colors.brown),
+                                  onPressed: () {
+                                    // Aksi edit di sini
+                                  },
+                                ),
+                                IconButton(
+                                  icon: const Icon(Icons.delete,
+                                      color: Colors.redAccent),
+                                  onPressed: () {
+                                    showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return AlertDialog(
+                                          title: const Text('Hapus Buku'),
+                                          content: const Text(
+                                              'Apakah Anda yakin ingin menghapus buku ini?'),
+                                          actions: [
+                                            TextButton(
+                                              onPressed: () =>
+                                                  Navigator.pop(context),
+                                              child: const Text('Batal'),
+                                            ),
+                                            TextButton(
+                                              onPressed: () {
+                                                deleteBook(book['id']);
+                                                Navigator.pop(context);
+                                              },
+                                              child: const Text('Hapus'),
+                                            ),
+                                          ],
+                                        );
+                                      },
+                                    );
+                                  },
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Navigasi ke halaman untuk menambah buku baru
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const AddBookPage(),
+            ),
+          );
+        },
+        backgroundColor: Colors.brown[400],
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class AddBookPage extends StatefulWidget {
+  const AddBookPage({super.key});
+
+  @override
+  _AddBookPageState createState() => _AddBookPageState();
+}
+
+class _AddBookPageState extends State<AddBookPage> {
+  final _titleController = TextEditingController();
+  final _authorController = TextEditingController();
+  final _descriptionController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  Future<void> addBook() async {
+    if (_formKey.currentState!.validate()) {
+      final title = _titleController.text;
+      final author = _authorController.text;
+      final description = _descriptionController.text;
+
+      try {
+        final response = await Supabase.instance.client.from('books').insert([
+          {
+            'title': title,
+            'author': author,
+            'description': description,
+          }
+        ]);
+
+        if (response.error == null) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Buku berhasil ditambahkan!')),
+          );
+          Navigator.pop(context); // Kembali ke halaman daftar buku
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Gagal menambahkan buku')),
+          );
+        }
+      } catch (e) {
+        print('Error adding book: $e');
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('')),
+        );
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tambah Buku'),
+        backgroundColor: Colors.brown[300],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                controller: _titleController,
+                decoration: const InputDecoration(
+                  labelText: 'Title',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Judul tidak boleh kosong';
+                  }
+                  return null;
+                },
               ),
-              );
-            }
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: _authorController,
+                decoration: const InputDecoration(
+                  labelText: 'Author',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Penulis tidak boleh kosong';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: _descriptionController,
+                decoration: const InputDecoration(
+                  labelText: 'Description',
+                  border: OutlineInputBorder(),
+                ),
+                maxLines: 4,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Deskripsi tidak boleh kosong';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: addBook,
+                child: const Text('Add'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.brown[300],
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
